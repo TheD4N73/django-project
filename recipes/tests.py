28f5d6ec449b0ecf1670e9b592d6785fcf57ab56
TheD4N73 <gustavoamaral.me@gmail.com>
2022-07-16 13:48:09 -0400

    reverse home url test

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
+from django.urls import reverse
 
 class RecipeURLsTest(TestCase):
-    def test_is_ok(self):
-        ...
+    def test_recipe_home_url(self):
+        home_url = reverse('recipes:home')
+        self.assertEqual(home_url, '/')
