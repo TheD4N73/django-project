c8462f64b1641734902f587132ac299f2a8dcfa8
TheD4N73 <gustavoamaral.me@gmail.com>
2022-10-22 11:20:06 -0400

    Import the http404 lib

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from django.shortcuts import render, get_list_or_404, get_object_or_404
 # from utils.recipes.factory import make_recipe
 from .models import *
@@ -36,4 +37,8 @@ def recipes(request, id):
 
 
 def search(request):
+    search_term = request.GET.get('q')
+
+    if not search_term:
+        raise Http404()
     return render(request, 'recipes/pages/search.html')
