29f0f6013d4804cd0d99b7ca37166d5b1c5dc7e3
TheD4N73 <gustavoamaral.me@gmail.com>
2022-10-22 14:35:30 -0400

    Defining queries to avoid ambiguity or security breach

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,6 @@
 from django.http import Http404
 from django.shortcuts import render, get_list_or_404, get_object_or_404
-# from utils.recipes.factory import make_recipe
+from django.db.models import Q
 from .models import *
 
 
@@ -41,6 +41,16 @@ def search(request):
 
     if not search_term:
         raise Http404()
+
+    recipes = Recipe.objects.filter(
+        Q(
+            Q(title__icontains=search_term) |
+            Q(description__icontains=search_term),
+        ),
+        is_published=True
+    ).order_by('-id')
+
     return render(request, 'recipes/pages/search.html', {
         'page_title': f'Search for "{search_term}"',
+        'recipes': recipes,
     })
