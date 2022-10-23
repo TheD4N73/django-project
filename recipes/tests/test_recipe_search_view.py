ad6bc9f0e7cd32bbf16c62f915e105b7749d40c1
TheD4N73 <gustavoamaral.me@gmail.com>
2022-10-23 13:19:54 -0400

    Setting up individual and collective search tests by title

--- recipes/tests/test_recipe_search_view.py
+++ recipes/tests/test_recipe_search_view.py
@@ -24,3 +24,33 @@ class RecipeSearchViewTest(RecipeTestBase):
             'Search for &quot;Teste&quot;',
             response.content.decode('utf-8')
         )
+
+    def test_recipe_search_can_find_recipe_by_title(self):
+        title1 = 'This is recipe one'
+        title2 = 'This is recipe two'
+
+        recipe1 = self.make_recipe(
+            slug='One',
+            title='This is recipe one',
+            author_data={ 'username': 'one'}
+        )
+
+        recipe2 = self.make_recipe(
+            slug='two',
+            title='This is recipe two',
+            author_data={'username': 'two'}
+        )
+
+        search_url = reverse('recipes:search')
+        response1 = self.client.get(f'{search_url}?search={title1}')
+        response2 = self.client.get(f'{search_url}?search={title2}')
+        response_both = self.client.get(f'{search_url}?search=this')
+
+        self.assertIn(recipe1, response1.context['recipes'])
+        self.assertNotIn(recipe2, response1.context['recipes'])
+
+        self.assertIn(recipe2, response2.context['recipes'])
+        self.assertNotIn(recipe1, response2.context['recipes'])
+
+        self.assertIn(recipe1, response_both.context['recipes'])
+        self.assertIn(recipe2, response_both.context['recipes'])
