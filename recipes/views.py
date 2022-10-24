3c06813877c857c4b73a2bc00cdb7100afac16aa
TheD4N73 <gustavoamaral.me@gmail.com>
2022-10-24 14:40:38 -0400

    Added pagination in template and view

--- recipes/views.py
+++ recipes/views.py
@@ -2,6 +2,7 @@ from django.db.models import Q
 from django.http import Http404
 from django.shortcuts import render, get_list_or_404, get_object_or_404
 from django.core.paginator import Paginator
+from utils.pagination import make_pagination_range
 from .models import *
 
 
@@ -11,12 +12,23 @@ def home(request):
             is_published=True,).order_by('-id')
     )
 
-    current_page = request.GET.get('page', 1)
+    try:
+        current_page = int(request.GET.get('page', 1))
+    except ValueError:
+        current_page = 1
+
     paginator = Paginator(recipes, 9)
     page_obj = paginator.get_page(current_page)
 
+    pagination_range = make_pagination_range(
+        paginator.page_range,
+        4,
+        current_page
+    )
+
     return render(request, 'recipes/pages/index.html', context={
         'recipes': page_obj,
+        'pagination_range': pagination_range
     })
 
 
