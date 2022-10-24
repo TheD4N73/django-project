2c7d85b106ca5818acf479b5a31fb7c972952e5e
TheD4N73 <gustavoamaral.me@gmail.com>
2022-10-24 11:33:19 -0400

    Pagination logic

--- utils/test_pagination.py
+++ utils/test_pagination.py
@@ -12,4 +12,31 @@ class PaginationTest(TestCase):
         self.assertEqual([1, 2, 3, 4], pagination)
 
     def test_first_range_is_static_if_current_page_is_less_than_middle_page(self):
-        ...
+        pagination = make_pagination_range(
+            page_range=list(range(1, 21)),
+            qty_pages=4,
+            current_page=1,
+        )
+        self.assertEqual([1, 2, 3, 4], pagination)
+
+        pagination = make_pagination_range(
+            page_range=list(range(1, 21)),
+            qty_pages=4,
+            current_page=2,
+        )
+        self.assertEqual([1, 2, 3, 4], pagination)
+
+        # HERE RANGE SHOULD CHANGE
+        pagination = make_pagination_range(
+            page_range=list(range(1, 21)),
+            qty_pages=4,
+            current_page=3,
+        )
+        self.assertEqual([2, 3, 4, 5], pagination)
+
+        pagination = make_pagination_range(
+            page_range=list(range(1, 21)),
+            qty_pages=4,
+            current_page=4,
+        )
+        self.assertEqual([3, 4, 5, 6], pagination)
