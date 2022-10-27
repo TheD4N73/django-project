a14f030db97a74b214a9b166f986e1499b2b5c50
TheD4N73 <gustavoamaral.me@gmail.com>
2022-10-27 15:29:28 -0400

    Dotenv import and basic configuration

--- recipes/views.py
+++ recipes/views.py
@@ -1,9 +1,11 @@
 from django.db.models import Q
 from django.http import Http404
 from django.shortcuts import render, get_list_or_404, get_object_or_404
-from django.core.paginator import Paginator
 from utils.pagination import make_pagination
-from .models import *
+from .models import Recipe
+import os
+
+PER_PAGE = os.environ.get('PER_PAGE', 9)
 
 
 def home(request):
@@ -12,7 +14,7 @@ def home(request):
             is_published=True,).order_by('-id')
     )
 
-    page_obj, pagination_range = make_pagination(request, recipes, 9)
+    page_obj, pagination_range = make_pagination(request, recipes, PER_PAGE)
 
     return render(request, 'recipes/pages/index.html', context={
         'recipes': page_obj,
@@ -27,7 +29,7 @@ def category(request, category_id):
             is_published=True).order_by('-id')
     )
 
-    page_obj, pagination_range = make_pagination(request, recipes, 9)
+    page_obj, pagination_range = make_pagination(request, recipes, PER_PAGE)
 
     return render(request, 'recipes/pages/category.html', context={
         'recipes': page_obj,
@@ -59,7 +61,7 @@ def search(request):
         is_published=True
     ).order_by('-id')
 
-    page_obj, pagination_range = make_pagination(request, recipes, 9)
+    page_obj, pagination_range = make_pagination(request, recipes, PER_PAGE)
 
     return render(request, 'recipes/pages/search.html', {
         'page_title': f'Search for "{search_term}"',
