098e6a38418e3e172efb8c50e23d37b5a45ff3f9
TheD4N73 <gustavoamaral.me@gmail.com>
2022-10-27 16:45:34 -0400

    Test the amount of recipes per page

--- recipes/tests/test_recipe_home_view.py
+++ recipes/tests/test_recipe_home_view.py
@@ -2,6 +2,7 @@ from django.urls import resolve, reverse
 from recipes import views
 from .test_recipe_base import RecipeTestBase
 from unittest import skip
+from unittest.mock import patch
 
 
 class RecipeHomeViewTest(RecipeTestBase):
@@ -35,7 +36,8 @@ class RecipeHomeViewTest(RecipeTestBase):
         self.assertIn('Recipe Title', content)
         self.assertEqual(len(response_context_recipes), 1)
 
-    @skip('Unresolved error but all work')
+
+    @skip
     def test_recipe_home_template_do_not_load_recipes_not_published(self):
         """If test recipe is_published False don't show"""
         self.make_recipe(is_published=False)
@@ -43,6 +45,22 @@ class RecipeHomeViewTest(RecipeTestBase):
         response = self.client.get(reverse('recipes:home'))
 
         self.assertIn(
-            'No recipes found here!',
+            '<h1>No recipes found here!</h1>',
             response.content.decode('utf-8')
         )
+
+
+    def test_recipe_home_is_paginated(self):
+        for i in range(8):
+            kwargs = {'slug': f'r{i}', 'author_data': {'username': f'u{i}'}}
+            self.make_recipe(**kwargs)
+
+        with patch('recipes.views.PER_PAGE', new=3):
+            response = self.client.get(reverse('recipes:home'))
+            recipes = response.context['recipes']
+            paginator = recipes.paginator
+
+            self.assertEqual(paginator.num_pages, 3)
+            self.assertEqual(len(paginator.get_page(1)), 3)
+            self.assertEqual(len(paginator.get_page(2)), 3)
+            self.assertEqual(len(paginator.get_page(3)), 2)
