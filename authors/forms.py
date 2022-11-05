cedd7312bbc6ca4672bc9b1fa9b4911007482ba8
TheD4N73 <gustavoamaral.me@gmail.com>
2022-11-05 10:53:22 -0400

    Feat: Using the clean_field method to validate a specific field

--- authors/forms.py
+++ authors/forms.py
@@ -1,10 +1,12 @@
 from django import forms
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 
 def add_attr(field, attr_name, attr_new_val):
     existing_attr = field.widget.attrs.get(attr_name, '')
     field.widget.attrs[attr_name] = f'{existing_attr} {attr_new_val}'.strip()
+
 
 def add_placeholder(field, placeholder_val):
     field.widget.attrs['placeholder'] = f'{placeholder_val}'.strip()
@@ -78,3 +80,27 @@ class RegisterForm(forms.ModelForm):
                 'placeholder': 'Type your password here',
             })
         }
+
+    def clean_password(self):
+        data = self.cleaned_data.get('password')
+
+        if 'atencao' in data:
+            raise ValidationError(
+                'Nao digite %(value)s no campo',
+                code='invalid',
+                params={'value': 'atencao'}
+            )
+
+        return data
+
+    def clean_first_name(self):
+        data = self.cleaned_data.get('first_name')
+
+        if 'John Doe' in data:
+            raise ValidationError(
+                'Nao digite %(value)s no campo',
+                code='invalid',
+                params={'value': 'John Doe'}
+            )
+
+        return data
