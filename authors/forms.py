0173e81b0fead7d230b909c5a36c3215dd81b49a
TheD4N73 <gustavoamaral.me@gmail.com>
2022-11-15 10:06:18 -0400

    Feat: Creation of single email logic in the database

--- authors/forms.py
+++ authors/forms.py
@@ -112,3 +112,12 @@ class RegisterForm(forms.ModelForm):
                     password_confirmation_error
                 ],
             })
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email', '')
+        exists = User.objects.filter(email=email).exists()
+
+        if exists:
+            raise ValidationError('User e-mail is already in use', code='invalid',)
+
+        return email
