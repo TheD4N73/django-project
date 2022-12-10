c092d5e99332be04f135cde63a136f2ad2a92fb4
d4n73 <gustavoamaral.me@gmail.com>
2022-12-10 18:10:49 -0400

    Fix: Test organization

--- tests/functional_tests/recipes/base.py
+++ tests/functional_tests/recipes/base.py
@@ -1,7 +1,6 @@
 import time
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from utils.browser import make_chrome_browser
-from selenium.webdriver.common.by import By
 
 
 class RecipeBaseFunctionalTest(StaticLiveServerTestCase):
@@ -15,10 +14,3 @@ class RecipeBaseFunctionalTest(StaticLiveServerTestCase):
 
     def sleep(self, seconds=5):
         time.sleep(seconds)
-
-
-class RecipeHomePageFunctionalTest(RecipeBaseFunctionalTest):
-    def test_recipe_home_page_without_recipes_error_not_found_message(self):
-        self.browser.get(self.live_server_url)
-        body = self.browser.find_element(By.TAG_NAME, 'body')
-        self.assertIn('No recipes found here!', body.text)
