e665821249c1de0bc62b78dbfabc0d2365d511d8
d4n73 <gustavoamaral.me@gmail.com>
2022-12-25 11:53:25 -0400

    Feat: URL to create a new recipe

--- authors/urls.py
+++ authors/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path('logout/', views.logout_view, name='logout'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('dashboard/recipe/<int:id>/edit/', views.dashboard_recipe_edit, name='dashboard_recipe_edit'),
+    path('dashboard/recipe/create/', views.dashboard_recipe_create_view, name='dashboard_recipe_create_view'),
 ]
