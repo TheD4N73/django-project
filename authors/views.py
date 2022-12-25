221e406d5b46a27480750794624c9da61fecf3b7
d4n73 <gustavoamaral.me@gmail.com>
2022-12-25 11:53:46 -0400

    Feat: View to create a new recipe

--- authors/views.py
+++ authors/views.py
@@ -122,13 +122,53 @@ def dashboard_recipe_edit(request, id):
 
     form = AuthorRecipeForm(
         data=request.POST or None,
-        instance=recipe
+        files=request.FILES or None,
+        instance=recipe,
     )
+
+    if form.is_valid():
+        recipe = form.save(commit=False)
+
+        recipe.author = request.user
+        recipe.preparation_steps_is_html = False
+        recipe.is_published = False
+        recipe.save()
+
+        messages.success(request, 'Your recipe has been saved successfully!')
+        return redirect(reverse('authors:dashboard_recipe_edit', args=(id,)))
 
     return render(
         request,
         'authors/pages/dashboard_recipe.html',
         context={
             'form': form
+        }
+    )
+
+
+@login_required(login_url='authors:login', redirect_field_name='next')
+def dashboard_recipe_create_view(request):
+    form = AuthorRecipeForm(
+        data=request.POST or None,
+        files=request.FILES or None,
+    )
+
+    if form.is_valid():
+        recipe = form.save(commit=False)
+
+        recipe.author = request.user
+        recipe.preparation_steps_is_html = False
+        recipe.is_published = False
+        recipe.save()
+
+        messages.success(request, 'Your recipe has been created successfully!')
+        return redirect(reverse('authors:dashboard', args=(recipe.id,)))
+
+    return render(
+        request,
+        'authors/pages/dashboard_recipe_create.html',
+        context={
+            'form': form,
+            'form_action': reverse('authors:dashboard_recipe_create_view')
         }
     )
