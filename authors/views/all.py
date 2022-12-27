bfccb5127d0a69f2f6b37c6f3ff35469999d31be
d4n73 <gustavoamaral.me@gmail.com>
2022-12-27 19:45:31 -0400

    Fix: Using the same editing class to create a new recipe

--- authors/views/all.py
+++ authors/views/all.py
@@ -1,6 +1,5 @@
 import os
 from authors.forms import RegisterForm, LoginForm
-from authors.forms.recipe_form import AuthorRecipeForm
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
@@ -105,71 +104,6 @@ def dashboard(request):
         context={
             'recipes': page_obj,
             'pagination_range': pagination_range,
-        }
-    )
-
-
-@login_required(login_url='authors:login', redirect_field_name='next')
-def dashboard_recipe_edit(request, id):
-    recipe = Recipe.objects.filter(
-        is_published=False,
-        author=request.user,
-        pk=id,
-    ).first()
-
-    if not recipe:
-        raise Http404()
-
-    form = AuthorRecipeForm(
-        data=request.POST or None,
-        files=request.FILES or None,
-        instance=recipe,
-    )
-
-    if form.is_valid():
-        recipe = form.save(commit=False)
-
-        recipe.author = request.user
-        recipe.preparation_steps_is_html = False
-        recipe.is_published = False
-        recipe.save()
-
-        messages.success(request, 'Your recipe has been saved successfully!')
-        return redirect(reverse('authors:dashboard_recipe_edit', args=(id,)))
-
-    return render(
-        request,
-        'authors/pages/dashboard_recipe.html',
-        context={
-            'form': form
-        }
-    )
-
-
-@login_required(login_url='authors:login', redirect_field_name='next')
-def dashboard_recipe_create_view(request):
-    form = AuthorRecipeForm(
-        data=request.POST or None,
-        files=request.FILES or None,
-    )
-
-    if form.is_valid():
-        recipe = form.save(commit=False)
-
-        recipe.author = request.user
-        recipe.preparation_steps_is_html = False
-        recipe.is_published = False
-        recipe.save()
-
-        messages.success(request, 'Your recipe has been created successfully!')
-        return redirect(reverse('authors:dashboard_recipe_edit', args=(recipe.id,)))
-
-    return render(
-        request,
-        'authors/pages/dashboard_recipe.html',
-        context={
-            'form': form,
-            'form_action': reverse('authors:dashboard_recipe_create_view')
         }
     )
 
