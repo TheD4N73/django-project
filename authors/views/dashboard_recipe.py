a9cd0f2bd45b370d5dee3db2050091e310560645
d4n73 <gustavoamaral.me@gmail.com>
2022-12-27 19:45:39 -0400

    Fix: Using the same editing class to create a new recipe

--- authors/views/dashboard_recipe.py
+++ authors/views/dashboard_recipe.py
@@ -8,7 +8,7 @@ from django.urls import reverse
 
 
 class DashboardRecipe(View):
-    def get_recipe(self, id):
+    def get_recipe(self, id=None):
         recipe = None
 
         if id:
@@ -32,13 +32,13 @@ class DashboardRecipe(View):
             }
         )
 
-    def get(self, request, id):
+    def get(self, request, id=None):
         recipe = self.get_recipe(id)
         form = AuthorRecipeForm(instance=recipe)
 
         return self.render_recipe(form)
 
-    def post(self, request, id):
+    def post(self, request, id=None):
         recipe = self.get_recipe(id)
         form = AuthorRecipeForm(
             data=request.POST or None,
@@ -55,6 +55,6 @@ class DashboardRecipe(View):
             recipe.save()
 
             messages.success(request, 'Your recipe has been saved successfully!')
-            return redirect(reverse('authors:dashboard_recipe_edit', args=(id,)))
+            return redirect(reverse('authors:dashboard_recipe_edit', args=(recipe.id,)))
 
         return self.render_recipe(form)
