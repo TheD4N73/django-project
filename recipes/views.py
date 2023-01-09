2c8d4729587bb54b634776eb4a477c755f146a03
d4n73 <gustavoamaral.me@gmail.com>
2023-01-09 19:44:44 -0400

    Feat: Add Debug Toolbar

--- recipes/views.py
+++ recipes/views.py
@@ -4,6 +4,7 @@ from django.db.models import Q
 from django.http import Http404, JsonResponse
 from django.views.generic import ListView, DetailView
 from utils.pagination import make_pagination
+from django.forms.models import model_to_dict
 
 PER_PAGE = int(os.environ.get('PER_PAGE', 9))
 
@@ -20,6 +21,7 @@ class RecipeListViewBase(ListView):
         qs = qs.filter(
             is_published=True,
         )
+        qs = qs.select_related('author', 'category')
         return qs
 
     def get_context_data(self, *args, **kwargs):
@@ -119,3 +121,24 @@ class RecipeDetailRecipe(DetailView):
             'is_detail_page': True,
         })
         return ctx
+
+
+class RecipeDetailApi(RecipeDetailRecipe):
+    def render_to_response(self, context, **response_kwargs):
+        recipe = self.get_context_data()['recipe']
+        recipe_dict = model_to_dict(recipe)
+
+        recipe_dict['created_at'] = str(recipe.created_at)
+        recipe_dict['updated_at'] = str(recipe.updated_at)
+
+        if recipe_dict.get('cover'):
+            recipe_dict['cover'] = self.request.build_absolute_uri() + recipe_dict['cover'].url[1:]
+        else:
+            recipe_dict['cover'] = ''
+
+        del recipe_dict['is_published']
+
+        return JsonResponse(
+            recipe_dict,
+            safe=False,
+        )
