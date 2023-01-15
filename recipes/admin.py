9a1b94879dfd5c392f90dd19903ef4a866238cfc
d4n73 <gustavoamaral.me@gmail.com>
2023-01-15 13:04:34 -0400

    Feat: Adding the tag in the recipes admin

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,12 +1,17 @@
 from django.contrib import admin
 from .models import *
+from tag.models import Tag
+from django.contrib.contenttypes.admin import GenericStackedInline
 
 
 class CategoryAdmin(admin.ModelAdmin):
     ...
 
 
-admin.site.register(Category, CategoryAdmin)
+class TagInline(GenericStackedInline):
+    model = Tag
+    fields = 'name',
+    extra = 1
 
 
 @admin.register(Recipe)
@@ -21,3 +26,6 @@ class RecipeAdmin(admin.ModelAdmin):
     prepopulated_fields = {
         "slug": ('title',)
     }
+    inlines = [
+        TagInline
+    ]
