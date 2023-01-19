99cc36e46987711024ad709119e3634feabfef70
TheD4N73 <gustavoamaral.me@gmail.com>
2023-01-19 14:03:13 -0500

    Fix: Reorganizing settings.py

---
+++ project/settings/environment.py
@@ -0,0 +1,35 @@
+import os
+from pathlib import Path
+from utils.environment import *
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ.get('SECRET_KEY', 'INSECURE')
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True if os.environ.get('DEBUG') == '1' else False
+
+ALLOWED_HOSTS = parse_comma_sep_str_to_list(get_env_variable('ALLOWED_HOSTS'))
+CSRF_TRUSTED_ORIGINS = parse_comma_sep_str_to_list(get_env_variable('CSRF_TRUSTED_ORIGINS'))
+
+ROOT_URLCONF = 'project.urls'
+
+WSGI_APPLICATION = 'project.wsgi.application'
+
+# Database
+# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': os.environ.get('DATABASE_ENGINE'),
+        'NAME': os.environ.get('DATABASE_NAME'),
+        'USER': os.environ.get('DATABASE_USER'),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
+        'HOST': os.environ.get('DATABASE_HOST'),
+        'PORT': os.environ.get('DATABASE_PORT'),
+    }
+}
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
---
+++ project/settings/messages.py
@@ -0,0 +1,10 @@
+from django.contrib.messages import constants
+
+
+MESSAGE_TAGS = {
+    constants.DEBUG: 'message-debug',
+    constants.INFO: 'message-info',
+    constants.WARNING: 'message-warning',
+    constants.SUCCESS: 'message-success',
+    constants.ERROR: 'message-error',
+}
