b0fc584d190985d6ede4c009679c2bb95df88bc2
d4n73 <gustavoamaral.me@gmail.com>
2023-01-21 10:46:56 -0400

    Fix: Fixing title_iexact for title __ixact

--- recipes/models.py
+++ recipes/models.py
@@ -70,7 +70,7 @@ class Recipe(models.Model):
         error_messages = defaultdict(list)
 
         recipe_from_db = Recipe.objects.filter(
-            title_iexact=self.title
+            title__iexact=self.title
         ).first()
 
         if recipe_from_db:
